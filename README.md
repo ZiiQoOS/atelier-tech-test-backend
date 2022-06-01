50ddfedaf68ef990dba14f6629f4e2ce8ec2fdb6
ZakariaESDModellium <ahmedzakariaesdairi@gmail.com>
2022-06-01 21:19:24 +0100

    Beatify Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Test Technique L’Atelier - Backend
+[![Build Status][build-shield]]()
+# Player Manager API
+
+> Player Manager API
+
+## Requirements
+
+> For development, you will only need [Node.js](https://nodejs.org/) and a node global package, [NPM](https://npmjs.org/) or [YARN](https://yarnpkg.com/), installed in your environement.
+## Install
+
+    $ git clone https://github.com/ZiiQoOS/atelier-tech-test-backend
+    $ cd atelier-tech-test-backend
+    $ npm install
+
+## Running the project
+
+To run the project use the command
+
+    $ npm run start
+
+To run test, use the command
+
+    $ npm test
+
+
+## Demo
+The project is deployed on [Heroku](https://heroku.com), you can find the live version on this link : [player-manager-api](https://atelier-tech-test-backend.herokuapp.com/)
+
+## Documentation
+The API documentation is available on [Docs](https://atelier-tech-test-backend.herokuapp.com/api-docs/), a swagger based documentation.
+
+## Built With
+Some of the major technologies used to bring this projet to life:
+* [Nodejs](https://nodejs.org/)
+* [Express](https://expressjs.com/)
+* [Swagger](https://swagger.io/)
--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const bodyParser = require("body-parser");
 const cors = require('cors');
 const express = require("express");
 
-const swaggerJdDoc = require('swagger-jsdoc');
+// swagger imports
+const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./src/swagger.json');
 
 
 const config = require('./config/config');
 const PlayersRoutes = require("./src/players/routes");
-
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 // Configure CORS
 app.use(cors(config.cors));
 app.use(bodyParser.urlencoded({
@@ -21,30 +22,35 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 const definition = swaggerDocument.swaggerDefinition;
 
+// Swagger config
 const options = {
-  definition,     
-  apis:["src/players/routes/*.js"]
+  definition,
+  apis: ["src/players/routes/*.js"]
 }
-const swaggerDocs = swaggerJdDoc(options);
+const swaggerDocs = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
 
-app.use("/", PlayersRoutes);
+// Endpoints
 // Home endpoint
-app.get("/", (req, res, next) => {
+app.get("/", async (req, res, next) => {
   res.status(200).json({
-    message: 'Hello there!'
+    message: 'Hello there, this the player manager api!'
   });
 });
+// Players endpoints
+app.use("/", PlayersRoutes);
 
-
-
-app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
-    message: err.message
-  })
-});
 
 app.listen(PORT, (req) => {
   console.log(`server is listening on http://localhost:${PORT}`);
+});
+
+// Catching uncaught errors
+process.on('uncaughtException', err => {
+  console.error('an uncaught exception', err)
+});
+
+process.on('unhandledRejection', err => {
+  console.error('an unhandled rejection', err)
 });
